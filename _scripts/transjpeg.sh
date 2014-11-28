329dc007b437e0ffaef0cc569e3d146e86559887
Liao PH <liaoishere@gmail.com>
2014-11-28 14:28:37 +0800

    update jpg to progressive format

---
+++ _scripts/transjpeg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Convert images to progressive jpeg format.
+
+DIR='/home/liao/Desktop/liaoishere.github.io/'
+
+for i in `find ${DIR}images -type f -name *.jpg`; do
+#	if [ "${i##*.}" != jpg ]; then
+#		convert "$i" "${i%.*}.jpg"
+#		rm -f "$i"
+#		i="${i%.*}.jpg"
+#	fi
+	jpegtran -copy none -optimize -outfile $i $i
+	jpegtran -copy none -progressive -outfile $i $i
+done
+
+# replace png|gif with progressive jpg
+#for i in `find ${DIR}_posts -type f -name *.md`; do
+#	sed -r -i 's@(!\[.*\]\(/images/.*)(\<png\>|\<gif\>)@\1jpg@g' $i
+#done
